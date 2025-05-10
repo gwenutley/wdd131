5b27e03dce802b3ab600f5b233c4d937f84733c6
gwenutley <gwen.utley24@gmail.com>
2025-05-10 15:36:28 -0600

    random fixes

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,7 @@
-const currentYear = document.querySelector("#currentyear"); // Note the #
+/*get the current year and place it in footer*/
+const currentYear = document.querySelector("#currentyear");
 currentYear.textContent = new Date().getFullYear();
 
+/*get last modified and place in footer*/
 let lastModified = new Date(document.lastModified);
 document.getElementById("lastModified").innerHTML = lastModified;
