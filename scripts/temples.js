84c8d07bef0fd146f70e64b103fb392f45adb1d5
gwenutley <gwen.utley24@gmail.com>
2025-05-16 19:43:34 -0600

    temples picture album, interactive menu

---
+++ scripts/temples.js
@@ -0,0 +1,16 @@
+
+const mainnav = document.querySelector('.menu')
+const hambutton = document.querySelector('#navigate');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
+
+/*get the current year and place it in footer*/
+const currentYear = document.querySelector("#currentyear");
+currentYear.textContent = new Date().getFullYear();
+
+/*get last modified and place in footer*/
+let lastModified = new Date(document.lastModified);
+document.getElementById("lastModified").innerHTML = lastModified;
