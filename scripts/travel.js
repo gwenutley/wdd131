3f0260e8073fbd8647b7f043551248f7a4a56181
gwenutley <gwen.utley24@gmail.com>
2025-06-18 11:32:46 -0600

    finished website project

--- scripts/travel.js
+++ scripts/travel.js
@@ -1,15 +1,17 @@
-/*get the current year and place it in footer*/
-const currentYear = document.querySelector("#currentyear");
-currentYear.textContent = new Date().getFullYear();
+document.addEventListener('DOMContentLoaded', function () {
 
-/*get last modified and place in footer*/
-let lastModified = new Date(document.lastModified);
-document.getElementById("lastModified").innerHTML = lastModified;
+    /*get the current year and place it in footer*/
+    const currentYear = document.querySelector("#currentyear");
+    currentYear.textContent = new Date().getFullYear();
+
+    /*get last modified and place in footer*/
+    let lastModified = new Date(document.lastModified);
+    document.getElementById("lastModified").innerHTML = lastModified;
 
 
 
-//products array and creting its options
-window.onload = function () {
+    //products array and creting its options
+    
     const destinations = [
         {
             name: "iceland",
@@ -42,40 +44,40 @@ window.onload = function () {
         option.textContent = destination.name;
         select.appendChild(option);
     });
-};
-
-
-/*populate the forms activities based on the destination selected*/
-const destinationChoice = document.getElementById('destination');
-const activities = document.getElementById('activities');
-
-const options = {
-    hawaii: ["Kailua Beach", "Pearl Harbor", "Diamond Head Crator"],
-    iceland: ["Reyinsfjara Beach", "Glacier Lagoon", "Reykjavik"],
-    paris: ["Eiffel Tower", "Notre-Dame", "Louvre Museum"],
-    guatemala: ["Antigua", "Lake Atitlan", "Pacaya Volcano"],
-    japan: ["Tokyo", "Mount Fuji", "Himeji Castle"],
-    mexicocity: ["Teotihuacan", "Chapultepec", "Xochimilco"],
-    netherlands: ["Amsterdam", "Keukenhof Gardens", "Utrecht"]
     
-};
 
 
-destinationChoice.addEventListener('change', () => {
-    const chosenDestination = destinationChoice.value;
+    /*populate the forms activities based on the destination selected*/
+    const destinationChoice = document.getElementById('destination');
+    const activities = document.getElementById('activities');
 
-    activities.innerHTML = "";
+    const options = {
+        hawaii: ["Kailua Beach", "Pearl Harbor", "Diamond Head Crator"],
+        iceland: ["Reyinsfjara Beach", "Glacier Lagoon", "Reykjavik"],
+        paris: ["Eiffel Tower", "Notre-Dame", "Louvre Museum"],
+        guatemala: ["Antigua", "Lake Atitlan", "Pacaya Volcano"],
+        japan: ["Tokyo", "Mount Fuji", "Himeji Castle"],
+        mexicocity: ["Teotihuacan", "Chapultepec", "Xochimilco"],
+        netherlands: ["Amsterdam", "Keukenhof Gardens", "Utrecht"]
+    
+    };
 
-    if (options[chosenDestination]) {
-        options[chosenDestination].forEach(activity => {
-            const option = document.createElement("option");
-            option.value = activity.toLowerCase().replace(/\s+/g, '-');
-            option.textContent = activity;
-            activities.appendChild(option);
-        });
-    }
+
+    destinationChoice.addEventListener('change', () => {
+        const chosenDestination = destinationChoice.value;
+
+        activities.innerHTML = "";
+
+        if (options[chosenDestination]) {
+            options[chosenDestination].forEach(activity => {
+                const option = document.createElement("option");
+                option.value = activity.toLowerCase().replace(/\s+/g, '-');
+                option.textContent = activity;
+                activities.appendChild(option);
+            });
+        }
+    });
 });
-
 
 
 
